1b75473e9bb3163dc5d82c7c2d0beb0f37b4e8a0
gershon <gershonv@163.com>
2018-05-28 16:27:33 +0800

    搭建基本开发环境

---
+++ webpack/搭建基本环境/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry:'./src/index.js',
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename: '[name].js'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.less$/,
+                // 因为这个插件需要干涉模块转换的内容，所以需要使用它对应的 loader
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'less-loader',
+                    ],
+                }),
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {},
+                    },
+                ],
+            },
+            // ...
+            // {
+            //     test: /\.css/,
+            //     include: [
+            //         path.resolve(__dirname, 'src'),
+            //     ],
+            //     use:ExtractTextPlugin.extract({
+            //         fallback: 'style-loader',
+            //         use: 'css-loader'
+            //     })
+            // },
+
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html', // 配置输出文件名和路径
+            template: 'src/index.html', // 配置文件模板
+        }),
+        new ExtractTextPlugin('[name].css'),
+    ],
+};
